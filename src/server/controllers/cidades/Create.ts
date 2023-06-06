79112cc2eead61d04f87bff168691e9ddf7ad02a
Gutemberg <gutembergunico10@gmail.com>
2023-06-06 15:38:04 -0300

    feat: yup installation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,14 +1,32 @@
 import { Request, Response } from 'express';
-
+import * as yup from 'yup';
 
 interface Icidade {
     nome: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const create = (req: Request<{}, {}, Icidade>, res: Response) => {
+const bodyValidation: yup.Schema<Icidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
 
-    console.log(req.body);
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const create = async (req: Request<{}, {}, Icidade>, res: Response) => {
+    let validatedData: Icidade | undefined = undefined;
+
+    try {
+        validatedData = await bodyValidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.json({
+            errors: {
+                default: yupError.message,
+            }
+        });
+    }
+
+    console.log(validatedData);
 
     return res.send('Create!');
 };
